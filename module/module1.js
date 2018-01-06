61fcd2a12f54a45a40117f005049362f03e9ab46
shinshin <shin@shinshinui-MacBook-Air.local>
2018-01-07 01:35:49 +0900

    module test

---
+++ module/module1.js
@@ -0,0 +1,11 @@
+var calc = new Object();
+
+calc.plus = function(a, b) {
+    return a + b;
+}
+
+calc.minus = function(a, b) {
+    return a - b;
+}
+
+module.exports = calc;
---
+++ module/module_test.js
@@ -0,0 +1,7 @@
+var calc = require('./module1');
+
+var a = 3;
+var b = 1;
+
+console.log('plus : %i', calc.plus(a, b));
+console.log('minus : %i', calc.minus(a, b));
